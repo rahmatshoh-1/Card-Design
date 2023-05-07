50c59c0b3bb7ea6f205cd0bf31245f82716b0f1c
rahmatshoh rahmonov <rahmatshoh.rahmonov@mail.ru>
2023-05-07 11:04:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      farro: ['Farro', 'sans-serif'],
+      roboto:['Roboto', 'sans-serif'],
+      libBas: ['Libre Baskerville', 'serif'],
+    },
+    // colors:{
+    //   'orangeLight':'#F3EAE3',
+    // },
+  },
+  plugins: [],
+}
+
